6c57b8647d55d9e0be5c042a189d377bbb430f76
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-07-28 11:50:58 +0500

    feat: create page

--- src/components/Items/Card/Page/Page.jsx
+++ src/components/Items/Card/Page/Page.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import './Page.scss';
 
-function Page({ isOpenPage }) {
+function Page({ card, isOpenPage, setIsOpenPage, setIsScroll }) {
+
+  const handleClickButtonClose = () => {
+    setIsOpenPage(false);
+    setIsScroll(false);
+  }
 
   return (
     <section className={"page" + (isOpenPage ? " page_open app_hidden" : "")}>
-      
+      <button
+        className="page__button-close"
+        onClick={handleClickButtonClose}
+      ></button>
+      <h2 className="page__title">{card.name}</h2>
+      <p className="page__subtitle">{card.tagline}</p>
+      <img className="page__img" src={card.image_url} alt={card.name}/>
+      <p className="page__abv">ABV: {card.abv}%</p>
+      <p className="page__pairing">FOOD PAIRING: {Array.from(card.food_pairing).join(", ")}</p>
+      <p className="page__text">DESCRIPTION: {card.description}</p>
     </section>
   );
 }
